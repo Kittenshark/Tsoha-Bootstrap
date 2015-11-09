31ba61f73b912ff0fd4136f95ffbb714d3279b5d
Emmi Kössi <Emmi Kssi>
2015-11-09 06:09:45 +0200

    sql lauseita

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,12 +1,13 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
 CREATE TABLE Kayttaja(
+    userId SERIAL PRIMARY KEY,
     username varchar(50),
-    PRIMARY KEY (username),
+    --SERIAL PRIMARY KEY(username),
     password varchar(50) NOT NULL,
     firstname varchar(50) NOT NULL,
     lasname varchar(50) NOT NULL,
-    email varchar(50 NOT NULL,
+    email varchar(50) NOT NULL,
 );
+
 CREATE TABLE Tuoteryhma(
     id SERIAL PRIMARY KEY,
     fname varchar(50) NOT NULL,
@@ -22,5 +23,27 @@ CREATE TABLE Tuote(
     orderIt boolean DEFAULT FALSE,
     reserve boolean DEFAULT FALSE,
 );
+
+CREATE TABLE Tilaus(
+    orderId SERIAL PRIMARY KEY,
+    price decimal NOT NULL,
+    orderDay TIMESTAMP,
+    arrivalAddress varchar(70),
+    billingAddress varchar(70),
+    product_id INTEGER REFERENCE Tuote(id),
+    orderer varchar(50) REFERENCE Kayttaja(username),
+);
+
+CREATE TABLE Varaus(
+    reserveId SERIAL PRIMARY KEY,
+    price decimal NOT NULL,
+    wayOfPayment varchar(50) NOT NULL,
+    orderDay TIMESTAMP,
+    collectDay TIMESTAMP,
+    product_id INTEGER REFERENCE Tuote(id),
+    orderer varchar(50) REFERENCE Kayttaja(username),
+);
+
+
 
     
